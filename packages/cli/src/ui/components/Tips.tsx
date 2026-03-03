a34ce556e908ce73558e7fe141b6e54c21b3b0da
Z User <z@container>
2026-03-03 02:14:09 +0000

    style: add spacing after Tips message in CLI
    - Add marginBottom to create visual spacing after Tips message
    - Use flexDirection='column' for proper layout

--- packages/cli/src/ui/components/Tips.tsx
+++ packages/cli/src/ui/components/Tips.tsx
@@ -29,11 +29,13 @@ export const Tips: React.FC = () => {
   }, []);
 
   return (
-    <Box marginLeft={2} marginRight={2} marginBottom={1}>
-      <Text color={theme.text.secondary}>
-        {t('Tips: ')}
-        {t(selectedTip)}
-      </Text>
+    <Box marginLeft={2} marginRight={2} marginBottom={1} flexDirection="column">
+      <Box marginBottom={1}>
+        <Text color={theme.text.secondary}>
+          {t('Tips: ')}
+          {t(selectedTip)}
+        </Text>
+      </Box>
     </Box>
   );
 };
